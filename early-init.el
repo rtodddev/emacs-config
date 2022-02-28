90de13e806b23bebc7ba56a14e298416f67e8f63
Ryan Todd <r.todd.dev@gmail.com>
2022-02-27 20:09:41 -0500

    Add .gitignore. Install straight, use-package.

--- early-init.el
+++ early-init.el
@@ -1,1 +1,25 @@
-;;; init.el --- Early init file -*- lexical-binding: t -*-
+;;; early-init.el --- Early init file -*- lexical-binding: t -*-
+
+;; Disable package.el
+(setq package-enable-at-startup nil)
+
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Install use-package
+(straight-use-package 'use-package)
+
+;; Configure straight to use use-package by default
+(use-package straight
+   :custom (straight-use-package-by-default t))
